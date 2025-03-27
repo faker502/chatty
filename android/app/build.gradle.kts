f63dc22f60bf8c7711f07cbc5c28ad504d000d7b
faker <faker.frontend@icloud.com>
2025-03-27 18:55:38 +0700

    build apk config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.chatty.chatty"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
